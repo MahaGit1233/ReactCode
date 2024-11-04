3563837a1d9882f704f8fb652dcff20d3ade1ad9
MahaGit1233 <maharush5409@gmail.com>
2024-11-04 19:29:33 +0530

    filter commit

---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./ExpenseFilter.css";
+
+const ExpenseFilter = (props) => {
+    const dropDownChangehandler = (event) => {
+        props.onChangeFilter(event.target.value);
+    };
+
+return (
+    <div className="expenses-filter">
+        <div className="expenses-filter__control">
+            <label htmlFor="filter">Filter by Year</label>
+            <select value={props.selected} id="filter" onChange={dropDownChangehandler}>
+                <option value='2023'>2023</option>
+                <option value='2024'>2024</option>
+                <option value='2025'>2025</option>
+            </select>
+        </div>
+    </div>
+)
+}
+
+export default ExpenseFilter;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpenseFilter from './ExpenseFilter';
+
+const Expenses = (props) => {
+    const [filteredYear,setFilteredYear] = useState('2023');
+
+    const changeFilterHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses=props.expenses.filter((expense)=>{
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    return (
+        <Card className="expenses">
+            <ExpenseFilter selected={filteredYear} onChangeFilter={changeFilterHandler}/>
+            {filteredExpenses.map((expense) => 
+                (<ExpenseItem
+                    key={expense.id}
+                    date={expense.date}
+                    title={expense.title}
+                    price={expense.price}
+                    location={expense.location}
+                ></ExpenseItem>)
+            )};
+        </Card>
+    );
+}
+
+export default Expenses;
